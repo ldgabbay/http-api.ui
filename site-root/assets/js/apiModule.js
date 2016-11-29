cbddad02867cfaf5cf2ccefac87a8aec9c73a0dc
Lynn Gabbay <gabbay@gmail.com>
2016-11-29 02:38:07 -0800

    removed absolute paths (so it works when installed in a sub-path)

--- site-root/assets/js/apiModule.js
+++ site-root/assets/js/apiModule.js
@@ -14,31 +14,31 @@
         $stateProvider
             .state('apiDeeplink3', {
                 url: '/:spec/:h1/:h2/:h3',
-                templateUrl: '/app/apiView.html',
+                templateUrl: 'app/apiView.html',
                 controller: 'apiController',
                 controllerAs: 'vm'
             })
             .state('apiDeeplink2', {
                 url: '/:spec/:h1/:h2',
-                templateUrl: '/app/apiView.html',
+                templateUrl: 'app/apiView.html',
                 controller: 'apiController',
                 controllerAs: 'vm'
             })
             .state('apiDeeplink1', {
                 url: '/:spec/:h1',
-                templateUrl: '/app/apiView.html',
+                templateUrl: 'app/apiView.html',
                 controller: 'apiController',
                 controllerAs: 'vm'
             })
             .state('apiDeeplink0', {
                 url: '/:spec',
-                templateUrl: '/app/apiView.html',
+                templateUrl: 'app/apiView.html',
                 controller: 'apiController',
                 controllerAs: 'vm'
             })
             .state('api', {
                 url: '/',
-                templateUrl: '/app/apiView.html',
+                templateUrl: 'app/apiView.html',
                 controller: 'apiController',
                 controllerAs: 'vm'
             });
