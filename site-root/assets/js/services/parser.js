15144d17c48ed6c8f6224d5436859c09f3f62f84
Lynn Gabbay <gabbay@gmail.com>
2017-03-28 13:51:25 -0700

    added support for spec standard 1.4.3

--- site-root/assets/js/services/parser.js
+++ site-root/assets/js/services/parser.js
@@ -28,10 +28,14 @@
                 obj.shortTextClass = 'primitive-ss';
                 obj.shortText = 'string';
 
-                obj.isExpandable = obj.criteria || obj.examples;
-                if (obj.isExpandable) {
+                if (obj.description || obj.criteria || obj.examples) {
+                    obj.isExpandable = true;
                     obj.shortText = 'string';
                     obj.extendedView = 'app/shared/schema-extended/general-ss.html';
+                } else {
+                    obj.isExpandable = false;
+                    obj.shortText = 'string';
+                    obj.extendedView = null;
                 }
             },
             exitReferenceSS: function(obj) {
@@ -55,72 +59,106 @@
 
                 obj.shortTextClass = 'reference-js';
                 obj.shortText = obj.ref;
+
                 obj.isExpandable = true;
                 obj.extendedView = 'app/shared/schema-extended/reference-js.html';
             },
             exitOneOfJS: function(obj) {
                 obj.shortTextClass = 'primitive-js';
                 obj.shortText = 'oneOf';
+
                 obj.isExpandable = true;
                 obj.extendedView = 'app/shared/schema-extended/one-of-js.html';
             },
             exitNullJS: function(obj) {
                 obj.shortTextClass = 'primitive-js';
                 obj.shortText = 'null';
-                obj.isExpandable = false;
-                obj.extendedView = null;
+
+                if (obj.description) {
+                    obj.isExpandable = true;
+                    obj.extendedView = 'app/shared/schema-extended/null-js.html';
+                } else {
+                    obj.isExpandable = false;
+                    obj.extendedView = null;
+                }
             },
             exitBooleanJS: function(obj) {
                 obj.shortTextClass = 'primitive-js';
                 obj.shortText = 'boolean';
-                obj.isExpandable = false;
-                obj.extendedView = null;
+
+                if (obj.description) {
+                    obj.isExpandable = true;
+                    obj.extendedView = 'app/shared/schema-extended/boolean-js.html';
+                } else {
+                    obj.isExpandable = false;
+                    obj.extendedView = null;
+                }
             },
             exitNumberJS: function(obj) {
                 obj.shortTextClass = 'primitive-js';
-                if (obj.criteria || obj.examples) {
+                obj.shortText = 'number';
+
+                if (obj.description || obj.criteria || obj.examples) {
                     obj.isExpandable = true;
-                    obj.shortText = 'number';
                     obj.extendedView = 'app/shared/schema-extended/number-js.html';
                 } else {
                     obj.isExpandable = false;
-                    obj.shortText = 'number';
                     obj.extendedView = null;
                 }
             },
             exitStringJS: function(obj) {
-                if (obj.format) {
-                    obj.shortTextClass = obj.format.shortTextClass;
-                    obj.shortText = obj.format.shortText;
-                    obj.isExpandable = obj.format.isExpandable;
-                    obj.extendedView = 'app/shared/schema-extended/string-js.html'; // TODO obj.format.extendedView;
-                } else {
+                if (obj.description) {
                     obj.shortTextClass = 'primitive-js';
                     obj.shortText = 'string';
-                    obj.isExpandable = false;
-                    obj.extendedView = null;
+                    obj.isExpandable = true;
+                    obj.extendedView = 'app/shared/schema-extended/string-js.html';
+                } else {
+                    if (obj.format) {
+                        obj.shortTextClass = obj.format.shortTextClass;
+                        obj.shortText = obj.format.shortText;
+                        obj.isExpandable = obj.format.isExpandable;
+                        obj.extendedView = 'app/shared/schema-extended/format-string-js.html'; // TODO obj.format.extendedView;
+                    } else {
+                        obj.shortTextClass = 'primitive-js';
+                        obj.shortText = 'string';
+                        obj.isExpandable = false;
+                        obj.extendedView = null;
+                    }
                 }
             },
             exitArrayJS: function(obj) {
                 obj.shortTextClass = 'primitive-js';
-                if (obj.criteria || obj.examples || obj.items.length !== 0) {
-                    obj.shortText = 'array';
-                    obj.isExpandable = true;
-                    obj.extendedView = 'app/shared/schema-extended/array-js.html';
-                } else {
-                    obj.shortText = 'array';
-                    obj.isExpandable = false;
-                    obj.extendedView = null;
+                obj.shortText = 'array';
+
+                if (obj.arrayType === 'simple') {
+                    // obj.shortTextClass = obj.items.shortTextClass;
+                    obj.shortText = "array<" + obj.items.shortText + ">";
+
+                    if (obj.description || obj.criteria || obj.examples || obj.items.isExpandable) {
+                        obj.isExpandable = true;
+                        obj.extendedView = 'app/shared/schema-extended/simple-array-js.html';
+                    } else {
+                        obj.isExpandable = false;
+                        obj.extendedView = null;
+                    }
+                } else if (obj.arrayType === 'record') {
+                    if (obj.description || obj.criteria || obj.examples || obj.items.length !== 0) {
+                        obj.isExpandable = true;
+                        obj.extendedView = 'app/shared/schema-extended/record-array-js.html';
+                    } else {
+                        obj.isExpandable = false;
+                        obj.extendedView = null;
+                    }
                 }
             },
             exitObjectJS: function(obj) {
                 obj.shortTextClass = 'primitive-js';
-                if (obj.criteria || obj.examples || obj.properties.length !== 0) {
-                    obj.shortText = 'object';
+                obj.shortText = 'object';
+
+                if (obj.description || obj.criteria || obj.examples || obj.properties.length !== 0) {
                     obj.isExpandable = true;
                     obj.extendedView = 'app/shared/schema-extended/object-js.html';
                 } else {
-                    obj.shortText = 'object';
                     obj.isExpandable = false;
                     obj.extendedView = null;
                 }
