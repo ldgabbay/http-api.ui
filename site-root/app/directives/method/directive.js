772808515def73b4c3c311144d40366f6e99a73b
Lynn Gabbay <gabbay@gmail.com>
2018-03-22 13:12:29 -0700

    fixed isEmpty (missing from missing vm)

--- site-root/app/directives/method/directive.js
+++ site-root/app/directives/method/directive.js
@@ -20,6 +20,11 @@
 
         function link(scope, iElement, iAttrs, controller, transcludeFn) {
             scope.requestParameterListTypes = ['path', 'query', 'header'];
+            scope.requestHasPath = scope.method.request.path && scope.method.request.path.length;
+            scope.requestHasQuery = scope.method.request.query && scope.method.request.query.length;
+            scope.requestHasHeader = scope.method.request.header && scope.method.request.header.length;
+            scope.requestHasBody = scope.method.request.body && scope.method.request.body.length;
+            scope.isEmpty = !(scope.requestHasPath || scope.requestHasQuery || scope.requestHasHeader || scope.requestHasBody);
         }
     }
 })();
