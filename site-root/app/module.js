4ab02c28548e2199babcb6cdac580addb8ffb16a
Lynn Gabbay <gabbay@gmail.com>
2018-03-25 20:34:36 -0700

    renamed controller files for consistency

--- site-root/app/module.js
+++ site-root/app/module.js
@@ -14,31 +14,31 @@
         $stateProvider
             .state('error', {
                 url: '/error',
-                templateUrl: 'app/controllers/error/errorView.html',
+                templateUrl: 'app/controllers/error/view.html',
                 controller: 'errorController',
                 controllerAs: 'vm'
             })
             .state('apiDeeplink3', {
                 url: '/:h1/:h2/:h3?src',
-                templateUrl: 'app/controllers/app/appView.html',
+                templateUrl: 'app/controllers/app/view.html',
                 controller: 'appController',
                 controllerAs: 'vm'
             })
             .state('apiDeeplink2', {
                 url: '/:h1/:h2?src',
-                templateUrl: 'app/controllers/app/appView.html',
+                templateUrl: 'app/controllers/app/view.html',
                 controller: 'appController',
                 controllerAs: 'vm'
             })
             .state('apiDeeplink1', {
                 url: '/:h1?src',
-                templateUrl: 'app/controllers/app/appView.html',
+                templateUrl: 'app/controllers/app/view.html',
                 controller: 'appController',
                 controllerAs: 'vm'
             })
             .state('apiDeeplink0', {
                 url: '/?src',
-                templateUrl: 'app/controllers/app/appView.html',
+                templateUrl: 'app/controllers/app/view.html',
                 controller: 'appController',
                 controllerAs: 'vm'
             });
